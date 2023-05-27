c4a76736e11ba444138097d1e6e6c121a9dcd68e
kingayg15 <134763503+kingayg15@users.noreply.github.com>
2023-05-27 10:50:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['19099906004', 'KingAY', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
